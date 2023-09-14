d3638b8bb8d614ee67b89a95932313915a0d91ab
devesh <devesh@servc.co.il>
2023-09-14 15:16:09 +0800

    added new random logic, cors issue persists

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
+
 
 def read_student_names(file_path):
     with open(file_path, 'r') as file:
@@ -10,10 +13,22 @@ def read_student_names(file_path):
 
 student_names_file = 'students.txt'
 student_names = read_student_names(student_names_file)
+i = 0
+
+@app.route("/start_class", methods=['POST'])
+def start_class():
+    random.shuffle(student_names)
+    global i
+    i = 0
+    response = jsonify({})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route("/random_student", methods=['GET'])
 def random_student():
-    selected_student = random.choice(student_names)
+    global i
+    selected_student = student_names[i]
+    i += 1
     response = jsonify({
         'message': 'Random student selected',
         'student_name': selected_student

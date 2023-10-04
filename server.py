2b7e13c6c509adc0fa376d56318ec58e01cc302d
devesh <devesh@servc.co.il>
2023-10-04 10:41:54 +0800

    fully implemented feature!

--- server.py
+++ server.py
@@ -1,38 +1,28 @@
 from flask import Flask, jsonify
 import random
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
-
 
 def read_student_names(file_path):
     with open(file_path, 'r') as file:
         student_names = file.read().splitlines()
     return student_names
 
-student_names_file = 'students.txt'
+student_names_file = 'students_myclass.txt'
 student_names = read_student_names(student_names_file)
+random.shuffle(student_names)
 i = 0
-
-@app.route("/start_class", methods=['POST'])
-def start_class():
-    random.shuffle(student_names)
-    global i
-    i = 0
-    response = jsonify({})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
 
 @app.route("/random_student", methods=['GET'])
 def random_student():
     global i
-    selected_student = student_names[i]
-    i += 1
+    if i >= len(student_names):
+        i = 0
     response = jsonify({
         'message': 'Random student selected',
-        'student_name': selected_student
+        'student_name': student_names[i]
     })
+    i += 1
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
